f8e4fdfe8706fc9af8641746524a0825e391cbec
JadielSilva11 <jdielcunha@gmail.com>
2024-07-30 09:16:08 -0300

    inicio da questão 11 lista 2

---
+++ Exercícios/Lista 2/q11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int qtd_dados, qtd_lados;
+    printf("Digite quantos dados deseja lancar: ");
+    scanf("%d", &qtd_dados);
+    printf("Agora digite a quantidade de lados do dado: ");
+    scanf("%d", &qtd_lados);
+    
+    return 0;
+}
+
