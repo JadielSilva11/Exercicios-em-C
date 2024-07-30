4497a12e82c714c37eed37e89e7ac57a5c581531
JadielSilva11 <jdielcunha@gmail.com>
2024-07-30 09:29:48 -0300

    função sorteio

--- Exercícios/Lista 2/q11.c
+++ Exercícios/Lista 2/q11.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+int sorteio(int dados, int lados);
 
 int main()
 {
@@ -12,3 +15,20 @@ int main()
     return 0;
 }
 
+int sorteio(int dados, int lados)
+{
+    srand(time(NULL)); 
+    
+    int sorteio;
+
+    if(lados == 4)
+    {
+        for(int i=1;i<=dados;i++)
+        {
+            if(i == dados)
+            {
+                sorteio = rand()% (4 * i) + i;
+            }
+        }
+    }
+}
