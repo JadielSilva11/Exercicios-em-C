cdd3654decd4156b1e4fbfad01ffd9af704b9397
JadielSilva11 <jdielcunha@gmail.com>
2024-09-16 10:50:15 -0300

    remove json, create folder Alocação Dinâmica and add some questions

---
+++ Exercícios/Alocação Dinâmica/q1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(NULL));
+
+    int *p = malloc(5 * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Falha ao alocar memoria!\n");
+    }
+
+    for(int i=0;i<5;i++)
+    {
+        p[i] = rand()% 5;
+
+        printf("%d\n", p[i]);
+    }
+
+    free(p);
+    p = NULL;
+
+    if(p == NULL)
+    {
+        printf("Memoria liberada e ponteiro livre!\n");
+    }
+    
+    return 0;
+}
